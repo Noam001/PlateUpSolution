67474ddf1246555f37a6bc58af7417da98574eb9
ariatia55222@outlook.co.il <ariatia55222@outlook.co.il>
2025-10-19 11:17:45 +0300

    aaa

---
+++ PlateUpWS/IDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+
+namespace PlateUpWS
+{
+    public interface IDBContext
+    {
+        void OpenConnection(); //פתיחת קשר עם מסד נתונים
+        void CloseConnection(); //סגירת קשר עם נתונים
+        void BeginTransaction(); //פותח חבילה של פעולות ובודק אם כל הפעולות יתבצעו, או כולם או אף אחד
+        void Commit(); //לאשר שכל הפעולות התבצעו
+        void RollBack(); //לבטל את כל הפעולות במידה ולא כולם התבצעו
+        int Delete(string sql); //מחזיר את כמות הדברים שנמחקו, לדוגמא בקשה למחוק 10 ספרים אז צריך להיות ערך מוחזר 10
+        int Insert(string sql);
+        int Update(string sql); 
+        DataReader Select (string sql); //אובייקט שיכול לשמור את הrecordset
+    }
+}
