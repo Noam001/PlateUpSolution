254c564704ebbadea0e9d88cb4bf99c01ef7aca9
ariatia55222@outlook.co.il <ariatia55222@outlook.co.il>
2025-11-16 10:17:29 +0200

    fff

---
+++ PlateUpWS/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace PlateUpWS
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        RepositoryFactory repositoryFactory;
+
+        public AdminController()
+        {
+            this.repositoryFactory = new RepositoryFactory();
+        }
+    }
+}
--- PlateUpWS/Controllers/ClientController.cs
+++ PlateUpWS/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models;
 
 namespace PlateUpWS
 {
@@ -13,8 +14,132 @@ namespace PlateUpWS
         {
             this.repositoryFactory = new RepositoryFactory();
         }
+        [HttpPost]
+        public string LoginGetId(string email, string password)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.ClientRepository.Login(email, password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
+        [HttpPost]
+        public bool UpdateProfile(Client client)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.ClientRepository.Update(client);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
+        [HttpPost]
+        public bool MakeAnOrder(Order order)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.OrderRepository.Create(order);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
+        [HttpPost]
+        public bool AddMealToOrder(string mealId, string orderId, int price, int quantity, string notes)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.OrderRepository.AddMealToOrder(mealId,orderId,price,quantity,notes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
+        [HttpGet]
+        public bool RemoveMealFromOrder(string mealId, string orderId)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.OrderRepository.RemoveMealFromOrder(mealId, orderId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
+        [HttpPost]
+        public bool CheckoutUpdateStatus(string orderId)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.OrderRepository.CheckoutUpdateStatus(orderId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
 
-        
-
+        [HttpPost]
+        public bool LeaveAReview(Review review)
+        {
+            try
+            {
+                this.repositoryFactory.ConnectDb();
+                return this.repositoryFactory.ReviewRepository.Create(review);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.repositoryFactory.DisconnectDb();
+            }
+        }
     }
 }
