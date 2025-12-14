10a67ee990702597e358be77b35bf768fdcba4e9
ariatia55222@outlook.co.il <ariatia55222@outlook.co.il>
2025-12-14 10:11:28 +0200

    dddd

---
+++ WebPlateUp/Controllers/GuestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPlateUp.Controllers
+{
+    public class GuestController : Controller
+    {
+        [HttpGet]
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
