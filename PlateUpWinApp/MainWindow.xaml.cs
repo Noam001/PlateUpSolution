3a53a8074b371b694cef538415d1da83ab8ebf4a
ariatia55222@outlook.co.il <ariatia55222@outlook.co.il>
2026-02-03 11:52:44 +0200

    1233333

--- PlateUpWinApp/MainWindow.xaml.cs
+++ PlateUpWinApp/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PlateUpWinApp.Frames;
 
 namespace PlateUpWinApp
 {
@@ -16,11 +17,27 @@ namespace PlateUpWinApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        ViewReports viewReports;
+        Login viewLogin;
+        bool loginSuccess;
         public MainWindow()
         {
             InitializeComponent();
+            UpdateLoginButton();
+            ViewReports();
         }
-
+        public void ViewReports()
+        {
+            if (this.viewReports == null)
+                this.viewReports = new ViewReports();
+            this.FrameContent.Content = this.viewReports;
+        }
+        public void ViewLogin()
+        {
+            if (this.viewLogin == null)
+                this.viewLogin = new Login();
+            this.FrameContent.Content = this.viewLogin;
+        }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)//שולח - מי שגרם לאירוע
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -28,10 +45,35 @@ namespace PlateUpWinApp
                 this.DragMove();
             }
         }
-
+        private void UpdateLoginButton()
+        {
+            if (loginSuccess)
+            {
+                this.btnLogout.Content = "Logout";
+            }
+            else
+            {
+                this.btnLogout.Content = "Login";
+            }
+        }
         private void btnLogout_Click(object sender, RoutedEventArgs e)
         {
-           
+           if (!loginSuccess)
+            {
+                //לאסוף קלט.
+                //לבדוק במסד נתונים
+                //אם מסד נתונים מחזיר אמת 
+            }
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FoodTypes_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
